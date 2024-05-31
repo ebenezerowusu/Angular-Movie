1194f89db135da63286a44113d0ece69302ce202
Owusu Ebenezer <ebenezershoot9@gmail.com>
2024-05-31 17:48:52 +0000

    Create the Layouts

--- frontEnd/src/app/features/static/routes.ts
+++ frontEnd/src/app/features/static/routes.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: '', component: HomeComponent }
 ];
 
