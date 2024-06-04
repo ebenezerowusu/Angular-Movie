851670157759d91379311548a9aa46d4b9927fc7
Owusu Ebenezer <ebenezershoot9@gmail.com>
2024-06-04 18:15:14 +0000

    Card(Cart button, price, title) done

--- frontEnd/src/app/features/static/home/home.component.ts
+++ frontEnd/src/app/features/static/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CarouselSlideComponent } from './components/carousel-slide/carousel-slide.component';
+import { CardComponent } from '../../../shared/components/products/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselSlideComponent],
+  imports: [CarouselSlideComponent,CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ frontEnd/src/app/shared/interface/product/card.interface.ts
@@ -0,0 +1,16 @@
+export interface Rating {
+    rating: number;
+    count: number;
+}
+
+export interface Card {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: Rating;
+    quantity: number;
+    subTotal: number;
+}
